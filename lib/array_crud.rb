ff8b23f1ca7e71ba49727c086cd18aa85994f008
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-19 11:36:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Dance with the Dead", "Daniel Deluxe", ""]
+  ["Dance with the Dead", "Daniel Deluxe", "Ronald Jenkees", "Professor Kliq"]
 end
 
 def add_element_to_end_of_array(array, element)
